34c512574ae444ea257e88f4c477939dacebf83b
Thomas Birai <ucollege23@gmail.com>
2015-09-21 04:59:29 +0000

    Temperature Exercise Completed

---
+++ temperature.js
@@ -0,0 +1,23 @@
+ 
+ 
+ var Temperature = {
+     
+     toFahrenheit: function (celsius) {
+        return celsius * (9/5) + 32;
+        
+     },
+     
+    toCelsius: function (fahrenheit) {
+        return (fahrenheit - 32) * (5/9)
+        
+    }
+    
+ };
+ 
+ var tempToday = 82 ;
+ 
+ var tempInCelsius = Math.round( Temperature.toCelsius(tempToday) );
+ 
+ var message = "Today's temperature is "+tempToday+"°F, which is "+tempInCelsius+"°C.";
+ 
+ console.log(message);
